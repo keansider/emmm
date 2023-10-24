0f3a1fcb34cdb24e844fea0ac014f2c6cd787501
keansider <14760795866@163.com>
2023-10-24 14:04:48 +0800

    init

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,29 +1,31 @@
 import pygame
 import sys
+from settings import Settings
 
 
 class AlienInvasion:
     def __init__(self):
         #初始化
         pygame.init()
+        self.settings=Settings()
 
-        self.screen=pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
         #设置背景色
-        self.bg_color=(230,230,230)
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         #开始游戏
         while True:
             #监听键盘和鼠标事件
             for event in pygame.event.get():
-                if event.type ==pygame.QUIT:
+                if event.type == pygame.QUIT:
                     sys.exit()
             #刷新屏幕
             self.screen.fill(self.bg_color)
             pygame.display.flip()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     #运行游戏
-    ai=AlienInvasion()
+    ai = AlienInvasion()
     ai.run_game()
