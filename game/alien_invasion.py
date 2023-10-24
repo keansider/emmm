5f6995354bae8c8e5cddb5d1dd5abdd0e0a98f89
keansider <14760795866@163.com>
2023-10-24 17:09:02 +0800

    init

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,29 +1,61 @@
 import pygame
 import sys
 from settings import Settings
-
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         #初始化
         pygame.init()
-        self.settings=Settings()
+        self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         #设置背景色
         self.bg_color = (230,230,230)
 
+        self.ship=Ship(self)
+
     def run_game(self):
         #开始游戏
         while True:
-            #监听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #刷新屏幕
-            self.screen.fill(self.settings.bg_color)
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        # 监听键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            #按下
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            #抬起
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            # 向右
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            # 向右
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        # 刷新屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     #运行游戏
---
+++ game/ship.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Ship:
+    def __init__(self,ai_game):
+        # 初始化飞机位置
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        # 加载图像
+        self.image = pygame.image.load('C:/Users/路小/PycharmProjects/emmm/game/plant.png')
+        self.rect = self.image.get_rect()
+        # 放在底部中央
+
+        self.settings = ai_game.settings
+
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        if self.moving_right and self.rect.x<1000:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.x>=0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
