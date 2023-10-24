7dace33a5dfa7c1c8620778b27269161a5dd4d2b
keansider <14760795866@163.com>
2023-10-24 13:59:34 +0800

    init

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -8,6 +8,9 @@ class AlienInvasion:
 
         self.screen=pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
+        #设置背景色
+        self.bg_color=(230,230,230)
+
     def run_game(self):
         #开始游戏
         while True:
@@ -16,6 +19,7 @@ class AlienInvasion:
                 if event.type ==pygame.QUIT:
                     sys.exit()
             #刷新屏幕
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
 
 if __name__=='__main__':
