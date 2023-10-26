282186bdfd6f8d7fcc51e6b40d597bc49adda3a8
keansider <14760795866@163.com>
2023-10-26 19:52:48 +0800

    init

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -19,7 +19,9 @@ class AlienInvasion:
 
         self.ship=Ship(self)
         self.bullets = pygame.sprite.Group()
+
         self.aliens = pygame.sprite.Group()
+        self._creat_fleet()
 
 
     def run_game(self):
@@ -63,7 +65,6 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        print(len(self.bullets))
 
     def _fire_bullet(self):
         #开火
@@ -80,12 +81,40 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _update_screen(self):
-        # 刷新屏幕
+        # 刷新屏幕显示
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
         pygame.display.flip()
+
+    def _creat_fleet(self):
+        alien = Alien(self)
+        alien_width,alien_height=alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        #计算行数
+        ship_height=self.ship.rect.height
+        available_space_y=(self.settings.screen_height-(3*alien_height)-ship_height)
+        number_rows=available_space_y//(2*alien_height)
+        #创建外星人群
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._creat_alien(alien_number,row_number)
+
+
+
+    def _creat_alien(self,alien_number,row_number):
+        alien = Alien(self)
+        alien_width,alien_height=alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y=alien.rect.height+2*alien.rect.height*row_number
+        self.aliens.add(alien)
+
+
 
 
 
--- game/settings.py
+++ game/settings.py
@@ -1,8 +1,8 @@
 class Settings:
     def __init__(self):
         # 屏幕设置
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1600
+        self.screen_height = 1000
         self.bg_color = (230, 230, 230)
         #飞船设置
         self.ship_speed=1.5
